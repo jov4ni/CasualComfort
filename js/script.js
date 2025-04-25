c25af69c29f83561189e4d5d16e7c6e325681d80
Geovane Aparecido da Silva <geovane.silva@ssparaiso.pmssp>
2025-04-25 11:25:33 -0300

    versao 2 com novos recursos de contagem

--- js/script.js
+++ js/script.js
@@ -28,7 +28,7 @@ function adcionar(){
     }
     for(let i in codigos){
         if (codigo === codigos[i]){
-            alert("Esse código já foi inserido");
+            alert("Esse código já foi inserido.");
             document.getElementById('input-txt').value = '';
             document.getElementById("input-txt").focus();
             return;
@@ -36,24 +36,34 @@ function adcionar(){
     }
     codigos.push(codigo);
     document.getElementById("relatorio").innerHTML += `<p>${codigo}</p>`;
-    document.getElementById('input-txt').value = '';
+    document.getElementById("input-txt").value = '';
     document.getElementById("input-txt").focus();
     document.getElementById("btn-del").disabled = false;
     document.getElementById("btn-imp").disabled = false;
     total++;
+    document.getElementById("total").innerHTML = `Total de etiquetas: ${total}`;
     console.log(codigos);
     return total;
 }
 
 function desfazer(){
-    let paragrafos = document.querySelectorAll(".form-main p");
+    let paragrafos = document.querySelectorAll(".fundo-relatorio p");
     if (paragrafos.length > 1){
         paragrafos[paragrafos.length -1].remove();
         codigos.pop();
         total--;
         console.log(codigos);
-        if(paragrafos.length ==2) document.getElementById("btn-del").disabled = true;
+        if(paragrafos.length ==2){
+            document.getElementById("btn-del").disabled = true;
+            document.getElementById("btn-imp").disabled = true;
+            document.getElementById("total").innerHTML = `Total de etiquetas: ${total}`;
+            document.getElementById("input-txt").focus();
+            return total;
+        }
     }
+    document.getElementById("btn-imp").disabled = false;
+    document.getElementById("total").innerHTML = `Total de etiquetas: ${total}`;
+    document.getElementById("input-txt").focus();
     return total;
 }
 
@@ -62,74 +72,70 @@ function imprimir(){
     const ano = data.getFullYear();
     const mes = data.getMonth()+1;
     const diaSemana = diasDaSemana[data.getDay()];
-    document.getElementById("fecho").innerHTML += `<hr>`;
-    document.getElementById("fecho").innerHTML += `<p>Total de etiquetas: ${total}</p>`;
-    document.getElementById("fecho").innerHTML += `<hr>`;
-    document.getElementById("fecho").innerHTML += `<p>${dia}/${mes}/${ano} ass.:</p>`;
-    document.getElementById("btn-add").disabled = true;
-    document.getElementById("btn-del").disabled = true;
     document.getElementById("btn-imp").disabled = true;
     telaimprimir();
 }
 
 function telaimprimir(){
     let conteudo = document.querySelectorAll(".fundo-relatorio p");
-    let rodape = document.querySelectorAll("#fecho p");
     let janelaImpressao = window.open("", "", `width=${screen.availWidth},height=${screen.availHeight},top=0,left=0`); // Abre uma nova janela
-
-    console.log(rodape[1]);
 
     if(janelaImpressao){
     
         janelaImpressao.document.write("<html>"); 
         janelaImpressao.document.write(`<head><title>Impressão</title></head>`);
         janelaImpressao.document.write(`<style>
-            body {
-                    font-family: 'Arial', sans-serif;
-                    padding: 40px;
-                    margin: 0;
-                    color: #333;
+            body{
+                font-family: 'Arial', sans-serif;
+                padding: 40px;
+                margin: 10px;
+                color: #003366;
+            }
+            table{
+                padding: 30px;
+                color: #003366;
+                width: 100%;
+                margin: 2rem auto;
                 }
-                table{
-                    padding: 50px;
-                    color: #003366;
-                    width: 100%;
-                    margin: 2rem auto;
-                    }
-                table tr td{
-                    padding: 20px;
+            table tr td{
+                padding: 10px;
+            }
+
+            h2 {
+            text-align: center;
+            }
+
+            @media print {
+                button {
+                    display: none;
                 }
 
-                h1, h2 {
-                    color: #003366;
+                body {
+                    padding: 0;
                 }
-
-                @media print {
-                    button {
-                        display: none;
-                    }
-
-                    body {
-                        padding: 0;
-                    }
-                };</style>`);
-        janelaImpressao.document.write("<body><h2>Relatório de envio</h2><table>");
-
-        let j=1;
+            };</style>`);
+        janelaImpressao.document.write("<body><h2>Relatório de envio</h2><h3>Códigos:<table>");
         
-        while (j < conteudo.length){
+        let copiaarray =[];
+        for(let i=1; i < conteudo.length; i++){
+            copiaarray.push(conteudo[i].textContent);
+        }
+        copiaarray.sort((a, b) => a - b);
+        
+        let j=0;
+        while (j < copiaarray.length){
             janelaImpressao.document.write("<tr>");
             for (let i=1; i <= 3; i++){
-                if(typeof conteudo[j]!== "undefined"){
-                    janelaImpressao.document.write(`<td>${conteudo[j].textContent}</td>`);
+                if(typeof copiaarray[j]!== "undefined"){
+                    janelaImpressao.document.write(`<td>${copiaarray[j]}</td>`);
                     j++;
                 }
             }
             janelaImpressao.document.write("</tr>");
         }
         janelaImpressao.document.write("</table>");
-        janelaImpressao.document.write(`<hr><p>${rodape[0].textContent}</p>`);
-        janelaImpressao.document.write(`<hr><p>${rodape[1].textContent}</p>`);
+        janelaImpressao.document.write(`<hr><p>Total de etiquetas: ${total}</p>`);
+        janelaImpressao.document.write(`<hr><p>assinatura:</p>`);
         janelaImpressao.document.write("</body></html>");
         janelaImpressao.document.close(); // Fecha o fluxo de escrita
     }
--- js/texte.js
+++ js/texte.js
@@ -1,29 +1,5 @@
-const array = [111, 22, 2, 111, 3, 3, 4, 4, 44, 4, 111];
-console.log(array.indexOf(44));
+const array = [111, 22, 2, 111, 3, 3, 4, 4, 44, 4, 111, "aaa", 10000, "abc"];
 
-let index = frutas.indexOf("banana");
-if (index !== -1) {
-    frutas.splice(index, 1); // remove 1 item na posição encontrada
-}
+let copia = [...array].sort((a, b) => a - b);
 
-function desfazer(){
-    let paragrafos = document.querySelectorAll(".form-main p");
-    if (paragrafos.length > 1){
-        const removido = paragrafos[paragrafos.length -1];
-        const valor = removido.textContent;
-        removido.remove();
-        remover(valor);
-        total--;
-        if(paragrafos.length ==2) document.getElementById("btn-del").disabled = true;
-    }
-    return total;
-}
-
-function remover(valor){
-    let remover = valor;
-    let index = codigos.indexOf(remover);
-    if (index !== -1) {
-        codigos.splice(index, 1); // remove 1 item na posição encontrada
-    }
-    console.log(codigos);
-}
+console.log(copia);
